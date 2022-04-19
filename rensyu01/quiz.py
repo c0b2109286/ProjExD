79424605db99933568822d57b77353fa0c3389d5
c0b2109286 <c0b2109286@edu.teu.ac.jp>
2022-04-19 15:59:42 +0900

    練習

--- rensyu01/quiz.py
+++ rensyu01/quiz.py
@@ -1,10 +1,11 @@
 import random
-quiz = [["サザエさんの旦那の名前は？","マスオ"],["カツオの妹の名前は？","ワカメ"],["タラオはカツオから見てどんな関係？","甥"]]
+quiz = ["サザエさんの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
+ans_list = [["マスオ","ますお"],["ワカメ","わかめ"],["甥","おい","甥っ子","おいっこ"]]
 num_list = [0,1,2]
 num = random.choice(num_list)
-print(quiz[num][0])
+print(quiz[num])
 ans = input("答えを入力")
-if ans == quiz[num][1]:
+if ans in ans_list[num]:
     print("正解")
 else:
     print("不正解")
