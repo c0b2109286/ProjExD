d05aaff2d5c56ae312d2eaf80c20b13b8976ed25
c0b2109286 <c0b2109286@edu.teu.ac.jp>
2022-04-19 17:37:28 +0900

    修正#3

--- rensyu01/quiz.py
+++ rensyu01/quiz.py
@@ -1,8 +1,7 @@
-import random
+from random import randint
 quiz = ["サザエさんの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
 ans_list = [["マスオ","ますお"],["ワカメ","わかめ"],["甥","おい","甥っ子","おいっこ"]]
-num_list = [0,1,2]
-num = random.choice(num_list)
+num = randint(0,3)
 print(quiz[num])
 ans = input("答えを入力")
 if ans in ans_list[num]:
