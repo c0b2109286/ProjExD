f2cc1804a9737ef58f63273f9bd159076120bf51
c0b2109286 <c0b2109286@edu.teu.ac.jp>
2022-04-19 16:41:56 +0900

    練習

--- rensyu01/quiz.py
+++ rensyu01/quiz.py
@@ -1,11 +1,19 @@
 import random
-quiz = ["サザエさんの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
-ans_list = [["マスオ","ますお"],["ワカメ","わかめ"],["甥","おい","甥っ子","おいっこ"]]
-num_list = [0,1,2]
-num = random.choice(num_list)
-print(quiz[num])
-ans = input("答えを入力")
-if ans in ans_list[num]:
-    print("正解")
+list = ['サザエの旦那の名前は?','カツオの妹の名前は?','タラオはカツオから見てどんな関係?']
+list1 = ['ますお','マスオ']
+list2 = ['ワカメ','わかめ']
+list3 = ['甥','おい','甥っ子','おいっこ']
+question = random.choice(list)
+print(question)
+answer = input('答えを入力してください')
+if question == 'サザエの旦那の名前は?':
+    if answer in list1:
+        print('正解')
+elif question == 'カツオの妹の名前は?':
+    if answer in list2:
+        print('正解')
+elif question == 'タラオはカツオから見てどんな関係?':
+    if answer in list3:
+        print('正解')
 else:
-    print("不正解")
+    print('不正解') 
