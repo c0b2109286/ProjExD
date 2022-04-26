1b85e7bea95f7f609d4efa485ef469c3c531b907
c0b2109286 <c0b2109286@edu.teu.ac.jp>
2022-04-26 16:25:28 +0900

    三角関数を追加しました

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -1,15 +1,21 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+from winreg import DeleteKey
+import math
 
 root = tk.Tk()
 root.title("tk")
 root.geometry("300x450")
 
 k = 0
-num_list = [7,8,9,4,5,6,1,2,3,0]
+l = 1
+n = 3
+num_list = [7,8,9,4,5,6,1,2,3,".",0,"00"]
+kigou_list = ["/","*", "-", "+"]
+sannkaku = ["sin", "cos", "tan"]
 
-entry = tk.Entry(justify = tk.RIGHT,width=10,font=("Times New Roman", 40))
-entry.grid(column = 0, row = 0, padx=10, pady=0, columnspan=100)
+entry = tk.Entry(justify = tk.RIGHT,width=10,font=("Times New Roman", 43))
+entry.grid(column = 0, row = 0, padx=2, pady=2, columnspan=100)
 
 def button__click(event):
     txt = event.widget["text"]
@@ -21,19 +27,51 @@ def get_Entry(event):
     entry.delete(0, tk.END)
     entry.insert(tk.END, ans)
 
+def delete_Entry(event):
+    entry.delete(0, tk.END)
+
+
+def sannkaku_keisann(event):
+    num = entry.get()
+    txt = event.widget["text"]
+    if txt == "sin":
+        num = math.sin(math.radians(int(num)))
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, num)
+    elif txt == "cos":
+        num = math.cos(math.radians(int(num)))
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, num)
+    elif txt == "tan":
+        num = math.tan(math.radians(int(num)))
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, num)
+
+
 for i in num_list:
-    button = tk.Button(root, text = i ,font = ("Times New Roman", 30))
-    button.grid(column = k%3, row = k//3+2, padx=0, pady=10)
+    button = tk.Button(root, text = i ,font = ("Times New Roman", 10),width=6, height = 4)
+    button.grid(column = k%3, row = k//3+2, padx=4, pady=2)
     button.bind("<1>",button__click)
     k += 1
 
-button = tk.Button(root, text = "+" ,font = ("Times New Roman", 30))
-button.grid(column = 3, row = 4, padx=0, pady=10)
-button.bind("<1>",button__click)
+for j in kigou_list:
+    button = tk.Button(root, text = j ,font = ("Times New Roman", 10),width=6, height = 4,background="gray")
+    button.grid(column = 3, row = l, padx=4, pady=2)
+    button.bind("<1>",button__click)
+    l += 1
 
-button1 = tk.Button(root, text = "=" ,font = ("Times New Roman", 30))
-button1.grid(column = 3, row = 5, padx=0, pady=10)
+for t in sannkaku:
+    button = tk.Button(root, text = t ,font = ("Times New Roman", 10),width=6, height = 4,background="gray")
+    button.grid(column = 4, row = n, padx=4, pady=2)
+    button.bind("<1>",sannkaku_keisann)
+    n += 1
 
+button1 = tk.Button(root, text = "=" ,font = ("Times New Roman", 10),width=6, height = 4, background="sky blue")
+button1.grid(column = 3, row = 5, padx=4, pady=2)
 button1.bind("<1>", get_Entry)
+
+button2 = tk.Button(root, text = "DEL" ,font = ("Times New Roman", 10),width=6, height = 4, background="hot pink")
+button2.grid(column = 4, row = 1, padx=4, pady=2)
+button2.bind("<1>", delete_Entry)
 
 root.mainloop()
