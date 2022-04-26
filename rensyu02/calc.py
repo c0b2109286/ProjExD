fc0b4fd8b91cc9fd8f791d254cf618f7e22bdcab
c0b2109286 <c0b2109286@edu.teu.ac.jp>
2022-04-26 14:28:43 +0900

    計算機

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -4,4 +4,19 @@ root = tk.Tk()
 root.title("tk")
 root.geometry("300x450")
 
+(x, y) = (0, 0)
+num_list = [9, 6, 3, 8, 5, 2, 7, 4, 1]
+def button_one(num, x, y):
+    button = tk.Button(root, text = num ,font = ("Times New Roman", 30))
+    button.grid(column = x, row = y)
+
+for i in num_list:
+    button_one(i, x, y)
+    if y == 2:
+        x += 1
+        y = 0
+    else:
+        y += 1
+
+button_one(0, 0, 4)
 root.mainloop()
