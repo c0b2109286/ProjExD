1f0690e03cb6bf2b58895f91206f689baa12bbe6
c0b2109286 <c0b2109286@edu.teu.ac.jp>
2022-04-26 14:54:22 +0900

    クリックした時の反応を付けました

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 
 root = tk.Tk()
 root.title("tk")
@@ -6,9 +7,15 @@ root.geometry("300x450")
 
 (x, y) = (0, 0)
 num_list = [9, 6, 3, 8, 5, 2, 7, 4, 1]
+
 def button_one(num, x, y):
-    button = tk.Button(root, text = num ,font = ("Times New Roman", 30))
+    button = tk.Button(root, text = num ,width=5, height = 5)
     button.grid(column = x, row = y, padx=10, pady=10)
+    button.bind("<1>",button__click)
+
+def button__click(event):
+    txt = event.widget["text"]
+    tkm.showinfo(txt, f"[{txt}]のボタンが押されました")
 
 for i in num_list:
     button_one(i, x, y)
@@ -19,4 +26,5 @@ for i in num_list:
         y += 1
 
 button_one(0, 0, 4)
+
 root.mainloop()
