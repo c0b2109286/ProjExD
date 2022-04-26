5f2d1786cc6c5b205a821c061e841f7a2df8bc0e
c0b2109286 <c0b2109286@edu.teu.ac.jp>
2022-04-26 14:56:24 +0900

    テキスト入力欄作りました

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -5,26 +5,20 @@ root = tk.Tk()
 root.title("tk")
 root.geometry("300x450")
 
-(x, y) = (0, 0)
-num_list = [9, 6, 3, 8, 5, 2, 7, 4, 1]
+k = 0
+num_list = [7,8,9,4,5,6,1,2,3,0]
 
-def button_one(num, x, y):
-    button = tk.Button(root, text = num ,width=5, height = 5)
-    button.grid(column = x, row = y, padx=10, pady=10)
-    button.bind("<1>",button__click)
+entry = tk.Entry(justify = tk.RIGHT,width=10,font=("Times New Roman", 40))
+entry.grid(column = 0, row = 0, padx=10, pady=0, columnspan=10)
 
 def button__click(event):
     txt = event.widget["text"]
     tkm.showinfo(txt, f"[{txt}]のボタンが押されました")
 
 for i in num_list:
-    button_one(i, x, y)
-    if y == 2:
-        x += 1
-        y = 0
-    else:
-        y += 1
-
-button_one(0, 0, 4)
+    button = tk.Button(root, text = i ,font = ("Times New Roman", 30))
+    button.grid(column = k%3, row = k//3+2, padx=0, pady=10)
+    button.bind("<1>",button__click)
+    k += 1
 
 root.mainloop()
