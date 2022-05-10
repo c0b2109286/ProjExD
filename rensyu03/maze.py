23d736eee9130a6b542932bf5c24d1e885ad220a
c0b2109286 <c0b2109286@edu.teu.ac.jp>
2022-05-10 13:31:04 +0900

    こうかとんインスタンス作成

--- rensyu03/maze.py
+++ rensyu03/maze.py
@@ -6,7 +6,8 @@ def canvas():
     cy = 400
     canvas = tk.Canvas(root, width=1500, height=900, background="black")
     canvas.place(x=0, y=0)
-
+    tori = tk.PhotoImage(file="fig/5.png")
+    canvas.create_image(cx, cy, image = tori, tag = "tori")
 
 if __name__ == "__main__":
     root = tk.Tk()
