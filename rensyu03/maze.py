76cae1b887db964ccdb3158a47034ef210bfea61
c0b2109286 <c0b2109286@edu.teu.ac.jp>
2022-05-10 13:32:10 +0900

    keyの初期化

--- rensyu03/maze.py
+++ rensyu03/maze.py
@@ -1,16 +1,17 @@
+from email.mime import image
 import tkinter as tk
 
-def canvas():
+def create_koukaton():
     global cx, cy, tori
     cx = 300
     cy = 400
-    canvas = tk.Canvas(root, width=1500, height=900, background="black")
-    canvas.place(x=0, y=0)
     tori = tk.PhotoImage(file="fig/5.png")
     canvas.create_image(cx, cy, image = tori, tag = "tori")
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("1500x900")
-    canvas()
+    canvas = tk.Canvas(root, width=1500, height=900, background="black")
+    canvas.place(x=0, y=0)
+    create_koukaton()
     root.mainloop()
