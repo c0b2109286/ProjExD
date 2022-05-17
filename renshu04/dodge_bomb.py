203ecd68d480bcaf64a2d13ad7f1ff41e4b9a14e
c0b2109286 <c0b2109286@edu.teu.ac.jp>
2022-05-17 14:09:06 +0900

    鳥描写

--- renshu04/dodge_bomb.py
+++ renshu04/dodge_bomb.py
@@ -5,6 +5,10 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screan = pg.display.set_mode((1600, 900))
 
+    tori_img = pg.image.load("fig/0.png")
+    tori_img_2X = pg.transform.rotozoom(tori_img, angle=0.0 ,scale=2.0)
+    screan.blit(tori_img_2X, (900, 400))
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -12,7 +16,7 @@ def main():
             pg.display.update()
 
             clock = pg.time.Clock()
-            clock.tick(1000)
+            clock.tick(100)
 
 
 if  __name__ == "__main__":
