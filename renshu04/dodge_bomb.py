dfbe2ee5a38a7c125204e121a0ac4dca2d7bc4a9
c0b2109286 <c0b2109286@edu.teu.ac.jp>
2022-05-17 14:09:42 +0900

    鳥移動

--- renshu04/dodge_bomb.py
+++ renshu04/dodge_bomb.py
@@ -1,26 +1,45 @@
+from tkinter import Canvas
 import pygame as pg
 import sys
 
 def main():
+    global cx, cy
+
     pg.display.set_caption("逃げろ！こうかとん")
     screan = pg.display.set_mode((1600, 900))
+    
 
     tori_img = pg.image.load("fig/0.png")
     tori_img_2X = pg.transform.rotozoom(tori_img, angle=0.0 ,scale=2.0)
-    screan.blit(tori_img_2X, (900, 400))
+    
 
     while True:
+        pg.display.update()
+        main_proc()
+        screan.fill("BLACK")
+        screan.blit(tori_img_2X, (cx, cy))
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-            
-            pg.display.update()
-
             clock = pg.time.Clock()
             clock.tick(100)
 
+def main_proc():
+    global cx, cy
+    key = pg.key.get_pressed()
+    if key[pg.K_UP]:
+        cy -= 1
+    elif key[pg.K_DOWN]:
+        cy += 1
+    elif key[pg.K_LEFT]:
+        cx -= 1
+    elif key[pg.K_RIGHT]:
+        cx += 1
+    
 
 if  __name__ == "__main__":
+    cx, cy = 900, 400
     pg.init()
     main()
     pg.quit()
     sys.exit()
+    
