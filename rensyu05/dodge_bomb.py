653678fadf9233a8439819098bfbd7f70d6f340e
c0b2109286 <c0b2109286@edu.teu.ac.jp>
2022-05-24 15:51:38 +0900

    Bombクラウス追加

--- rensyu05/dodge_bomb.py
+++ rensyu05/dodge_bomb.py
@@ -1,5 +1,3 @@
-from cmath import rect
-from turtle import width
 import pygame as pg
 import sys
 import random
@@ -17,47 +15,71 @@ class Screan:       #Screenクラス（スクリーン描写）
 class Bird:       #Birdクラス（とり描写）
     key_data = {"K_UP":-1, "K_DOWN":1, "K_LEFT":-1, "K_RIGHT":1}
 
-    def __init__(self, tori_name = "fig/6.png", rate = 2, x = 900, y = 400):
+    def __init__(self, tori_name, rate, x, y):
         # rate：拡大率
-        self.x = x        #とりのｘ座標
-        self.y = y        #とりのｙ座標
         self.image = pg.transform.rotozoom(pg.image.load(tori_name), angle=0, scale=rate)           #とり画像の拡大
         self.rect = self.image.get_rect()       #Rect取得
+        self.rect.center = x, y
 
         
-    def update(self):
+    def update(self):       #こうかとんが外に出ないようにする関数
         key = pg.key.get_pressed()          #押されたキーを取得
 
         if key[pg.K_UP]:
-            self.y += int(Bird.key_data["K_UP"])
+            self.rect.centery += int(Bird.key_data["K_UP"])
         elif key[pg.K_DOWN]:
-            self.y += int(Bird.key_data["K_DOWN"])
+            self.rect.centery += int(Bird.key_data["K_DOWN"])
         elif key[pg.K_LEFT]:
-            self.x += int(Bird.key_data["K_LEFT"])
+            self.rect.centerx += int(Bird.key_data["K_LEFT"])
         elif key[pg.K_RIGHT]:
-            self.x += int(Bird.key_data["K_RIGHT"])
+            self.rect.centerx += int(Bird.key_data["K_RIGHT"])
 
-        if self.x < 0:
-            self.x += 1
-        elif self.x > 1500:
-            self.x -= 1
-        elif self.y < 0:
-            self.y += 1
-        elif self.y > 770:
-            self.y -=1
+        if self.rect.centerx  < 0:
+            self.rect.centerx += 1
+        elif self.rect.centerx  > 1500:
+            self.rect.centerx  -= 1
+        elif self.rect.centery < 0:
+            self.rect.centery += 1
+        elif self.rect.centery > 770:
+            self.rect.centery -=1
 
+class Bomb():
+    def __init__(self, color, hankei, speed_x, speed_y, screen):
+        # hakei : ボールの半径, speed_x : ｘ方向へのボールの速さ , speed_y : ｘ方向へのボールの速さ, screen : screenオブジェクト
+        self.image = pg.Surface((20, 20))
+        pg.draw.circle(self.image , color, (10, 10), hankei)
+        self.rect = self.image.get_rect()
+        self.image.set_colorkey((0,0,0))
+        self.rect.centerx =   random.randint(0, screen.rect.width)           #爆弾のｘ座標（ランダム）
+        self.rect.centery =   random.randint(0, screen.rect.height)          #爆弾のｙ座標（ランダム）
+        self.vx = speed_x
+        self.vy = speed_y
 
+    def update(self):
+        self.rect.move_ip(self.vx, self.vy)
+
+        if self.rect.centerx < 0 or self.rect.centerx > 1580:
+            self.vx *= -1
+        if self.rect.centery < 0 or self.rect.centery > 880:
+            self.vy *= -1
 
 def main():
     screan = Screan()       #コンストラクタを呼び出す
-    bird = Bird()
+    bird = Bird("fig/6.png", 2, 900, 400)
+    bomb = Bomb((255, 0, 0), 10, 1, 1, screan)
 
     while True:
         pg.display.update()
         screan.disp.blit(screan.image, (0,0))       #背景画像の貼り付け (0, 0)
-        bird.rect.center = bird.x, bird.y
-        screan.disp.blit(bird.image, bird.rect)
-        bird.update()
+
+        screan.disp.blit(bird.image, bird.rect)     #とり画像の貼り付け
+        bird.update()                               #とり座標のアップデート
+        
+        screan.disp.blit(bomb.image, bomb.rect)     #爆弾画像の貼り付け
+        bomb.update()                               #爆弾座標のアップデート
+
+        if bird.rect.colliderect(bomb.rect):        #とりが爆弾に衝突したとき
+            return                                  #whileを抜ける
 
         for event in pg.event.get():
             if event.type == pg.QUIT: return        
